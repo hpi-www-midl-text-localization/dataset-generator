d060962b113288d8c67d792029a188ad34dde11e
Jona Otholt <Jona.Otholt@hpi.uni-potsdam.de>
2018-11-16 18:11:05 +0100

    Add method to create multiple images

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def render_text_and_return_aabb(draw, xy, text, font=None, color=(255, 255, 255)
     width, height = font.getsize(text)
     draw.text(xy, text, font=font, fill=color)
 
-    return (xy[0], xy[1], xy[0] + width, xy[1] + height)
+    return xy[0], xy[1], xy[0] + width, xy[1] + height
 
 
 def render_random_text_at_random_position_and_return_aabb(image,
@@ -38,7 +38,7 @@ def render_random_text_at_random_position_and_return_aabb(image,
 
     text_length = randrange(min_text_length, max_text_length + 1)
     characters = string.ascii_letters + string.digits
-    text = ''.join([choice(characters) for i in range(text_length)])
+    text = ''.join([choice(characters) for _ in range(text_length)])
 
     font_size = randrange(min_font_size, max_font_size + 1, 2)  # use only even font sizes
     font = ImageFont.truetype("fonts/open-sans/OpenSans-Regular.ttf", font_size)
@@ -48,15 +48,27 @@ def render_random_text_at_random_position_and_return_aabb(image,
     return render_text_and_return_aabb(draw, (x, y), text, font=font, color=text_color)
 
 
+def create_images_with_text_and_bounding_box(n, width, height):
+    """Creates n images with text on them. The images will have the
+    dimensions width x height. Returns generated images and the
+    bounding boxes of the text on them.
+    """
+    images = []
+    bounding_boxes = []
+
+    for _ in range(0, n):
+        image = Image.new("RGB", (width, height))
+
+        rand_color = (randrange(255), randrange(255), randrange(255))
+
+        bounding_boxes.append(render_random_text_at_random_position_and_return_aabb(image, text_color=rand_color))
+        images.append(image)
+
+    return images, bounding_boxes
+
+
 image_width = 256
 image_height = 256
 
-image = Image.new("RGB", (image_width, image_height))
+images, bounding_boxes = create_images_with_text_and_bounding_box(10, image_width, image_height)
 
-rand_color = (randrange(255), randrange(255), randrange(255))
-
-top_left_x, top_left_y, bottom_right_x, bottom_right_y = render_random_text_at_random_position_and_return_aabb(image, text_color=rand_color)
-
-print(top_left_x, top_left_y, bottom_right_x, bottom_right_y)
-
-image.show()
