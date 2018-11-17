93fa414390f3f4893de24bec1ee416007c4dcf22
Milan Proell <milan.proell@student.hpi.de>
2018-11-17 19:06:35 +0100

    fixing test

--- test_main.py
+++ test_main.py
@@ -1,11 +1,11 @@
 import pytest
-from main import *
-from PIL import Image
+from main import calculate_word_aabb
+from PIL import Image, ImageFont
 
 def test_testenv():
-    assert(True)
+    assert True
 
 def test_calculate_word_aabb():
     font = ImageFont.truetype("fonts/open-sans/OpenSans-Regular.ttf", 12)
     image = Image.new("RGB", (100, 100))
-    assert(calculate_word_aabb("test", (0,0),font,image),((0, 0, 21, 13), 21))
+    assert calculate_word_aabb("test", (0,0),font,image) == ((0, 0, 21, 13), 21)
